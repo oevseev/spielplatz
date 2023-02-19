6b369a05a971fab82a69e4d134b910d75992b153
Oleg Evseev <git@x3n.me>
2023-02-20 02:55:38 +0400

    Make step_simulation play well with FixedTimestep

--- src/simulation/systems.rs
+++ src/simulation/systems.rs
@@ -15,5 +15,10 @@ pub(crate) fn unset_physics_pending(mut simulation_state: ResMut<SimulationState
 }
 
 pub(crate) fn step_simulation(mut simulation_state: ResMut<SimulationState>) {
+    // TODO: Move check from here after multiple run criteria are implemented in Bevy
+    if simulation_state.physics_pending {
+        return;
+    }
+
     simulation_state.physics_pending = true
 }
