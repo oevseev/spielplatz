6a36180abb8c2ff9cc047d9656851aa0902e0706
Oleg Evseev <git@x3n.me>
2023-02-20 00:43:31 +0400

    Schedule physics stages manually

--- src/simulation/systems.rs
+++ src/simulation/systems.rs
@@ -1,26 +1,19 @@
-use bevy::{prelude::*, ecs::schedule::ShouldRun};
-use bevy_rapier3d::prelude::*;
+use bevy::{ecs::schedule::ShouldRun, prelude::*};
 
-pub(crate) fn run_when_physics_pending(
-    time: Res<Time>,
-    rapier_configuration: Res<RapierConfiguration>,
-    sim_to_render_time: Res<SimulationToRenderTime>,
-) -> ShouldRun {
-    if let TimestepMode::Interpolated { dt, .. } = rapier_configuration.timestep_mode {
-        // Two assumptions here:
-        // - time is updated in CoreStage::Update and thus will be the same in the later stages,
-        // including SimulationStages::StepSimulation and the PhysicsStages::StepSimulation
-        // - the relative ordering of the stages is CoreStage::Update ->
-        // SimulationStages::StepSimulation -> PhysicsStages::StepSimulation
-        //
-        // For more information take a look at step_simulation in bevy_rapier/src/plugin/context.rs
+use crate::simulation::plugin::SimulationState;
 
-        if sim_to_render_time.diff + time.delta_seconds() > dt {
-            ShouldRun::Yes
-        } else {
-            ShouldRun::No
-        }
+pub(crate) fn run_if_physics_pending(simulation_state: Res<SimulationState>) -> ShouldRun {
+    if simulation_state.physics_pending {
+        ShouldRun::Yes
     } else {
-        panic!("can't use run_when_system_pending with timestep_mode other than TimestepMode::Interpolated");
+        ShouldRun::No
     }
+}
+
+pub(crate) fn unset_physics_pending(mut simulation_state: ResMut<SimulationState>) {
+    simulation_state.physics_pending = false
+}
+
+pub(crate) fn step_simulation(mut simulation_state: ResMut<SimulationState>) {
+    simulation_state.physics_pending = true
 }
